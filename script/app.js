b9a068eae1fc4d8cc8affa0e35bfe73068141285
Rexie18 <ro@students.kiron.ngo>
2017-03-14 19:07:00 +0100

    Update app.js

--- script/app.js
+++ script/app.js
@@ -8,45 +8,23 @@
       function MyLunchCheckController($scope){
      $scope.MenuInput = [];
      $scope.values = new Array($scope.MenuInput.length);
+     $scope.values.splice("");
+     $scope.message1 = "First message";
+     $scope.message2 = "Second message";
 
   };  
   
-})();
-
-
-// <body ng-controller="MyController">
-//     <label ng-repeat-start="label in labels">Label {{label}}</label>
-//     <input ng-model='values[$index]' type='text' value='' />
-//     <br ng-repeat-end>
-
-//     <button ng-click="saveEverything()">Save</button>
-//   </body>
+  $scope.check = function() {
+    $scope.values = [];
+    for (i = 0; i < $scope.MenuInput.length; i++) {
+     if($scope.values <= 3) {
+        return $scope.message1;
+      }else{
+        return $scope.message2;
+        
+         }
+      }
+        
+  });
   
-//   var myApp = angular.module('app', []);
-
-// myApp.controller('MyController', function($scope, $log) {
-//   $scope.labels = ['one', 'two', 'three'];
-//   $scope.values = new Array($scope.labels.length);
-
-//   $scope.saveEverything = function() {
-//     $scope.contents = [];
-//     for (i = 0; i < $scope.labels.length; i++) {
-//       $scope.contents.push({
-//         label: $scope.labels[i],
-//         value: $scope.values[i]
-//       });
-//     }
-//     $log.info('Saved to array[0]: ' + $scope.contents[0].value);
-//     $log.info('Saved to array[1]: ' + $scope.contents[1].value);
-//     $log.info('Saved to array[2]: ' + $scope.contents[2].value);
-//   }
-// });
-
-// $scope.check = function(value) {
-//     if (input <= $scope.values[i]) {
-//       return '270px';
-//     } else {
-//       return '360px';
-//     }
-//   };
-// });
+})();
