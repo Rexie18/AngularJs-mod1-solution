abf49e3ee9b17b0ca3c580f4a2a35f6ead470039
Rexie18 <ro@students.kiron.ngo>
2017-03-16 18:04:37 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 
-(function (angular) {
+(function () {
     'use strict';
-  MyLunchCheckcontroller.$inject  =  ['$scope'];
-  angular.module('LunchCheck', [])
-    .controller('MyLunchCheckcontroller', MyLunchCheckcontroller);
-  
-    var MyLunchCheckcontroller = function ($scope) {
+     
+    angular.module('LunchChecker', [])
+   .controller('MyLunchCheckController', MyLunchCheckController);
+     
+    MyLunchCheckController.$inject  =  ['$scope'];
+    function MyLunchCheckController($scope) {
       $scope.MenuInput  =  [];
       $scope.values  =  new Array($scope.MenuInput.length);
       $scope.values.splice("", "");
@@ -13,34 +14,12 @@
       $scope.msg2  =   "Too much!";
        
       $scope.checkInput  =  function() {
-        return $scope.values.length() <= 3 ? $scope.msg1 : $scope.msg2;
-       
-    }
+         return $scope.values.length() <= 3 ? $scope.msg1 : $scope.msg2;       
+      }
+     
   };
-  
-})(angular); 
-
-
-// (function () {
-//     'use strict';
-     
-//     angular.module('LunchCheck', [])
-//    .controller('MyLunchCheckController', MyLunchCheckController);
-     
-//     MyLunchCheckController.$inject  =  ['$scope'];
-//     function MyLunchCheckController($scope) {
-//       var msg1  =   "Enjoy!";
-//       var msg2  =   "Too much!";
-       
-//       this.checkInput  =  function() {
-//          $scope.MenuInput.split(",")  =  [];
-//         var InputReturn = $scope.MenuInput.split(",");
-//         return InputReturn.length() <= 3 ? msg1 : msg2;       
-//       }
-  
-//   };
       
-// })();
+})();
 
 
 // (function () {
