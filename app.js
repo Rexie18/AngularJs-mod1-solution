14341ceb59d501e182310a0271d63cd840b0f305
Rexie18 <ro@students.kiron.ngo>
2017-03-17 18:19:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -11,9 +11,13 @@
 //     $scope.MenuInput= [];
 //      $scope.msg1  =   "Enjoy!";
 //      $scope.msg2  =   "Too much!";
+//         $scope.msg3  =   "Enter data first";
 
 //      $scope.checkInput  =  function() {
-//             if($scope.MenuInput.split(",").length <= 3) {
+//               if(!$scope.MenuInput.split(",").length <= 3){
+//                 $scope.values = $scope.msg3;
+//             }
+//             else if($scope.MenuInput.split(",").length <= 3) {
 //                 $scope.values = $scope.msg1;
 //             } else{
 //                 $scope.values = $scope.msg2;
@@ -48,4 +52,4 @@
 //       };   
       
 // })();
-!function(){"use strict";function a(a){a.MenuInput=[],a.msg1="Enjoy!",a.msg2="Too much!",a.checkInput=function(){a.MenuInput.split(",").length<=3?a.values=a.msg1:a.values=a.msg2}}angular.module("LunchChecker",[]).controller("MyLunchCheckController",a),a.$inject=["$scope"]}();
+!function(){"use strict";function a(a){a.MenuInput=[],a.msg1="Enjoy!",a.msg2="Too much!",a.msg3="Enter data first",a.checkInput=function(){!a.MenuInput.split(",").length<=3?a.values=a.msg3:a.MenuInput.split(",").length<=3?a.values=a.msg1:a.values=a.msg2}}angular.module("LunchChecker",[]).controller("MyLunchCheckController",a),a.$inject=["$scope"]}();
