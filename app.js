20dfb736ebceaae616f885263c375326df321faf
Rexie18 <ro@students.kiron.ngo>
2017-03-17 18:26:34 +0100

    Update app.js

--- app.js
+++ app.js
@@ -28,28 +28,4 @@
 
 
 
-//     'use strict';
-     
-//     angular.module('LunchChecker', [])
-//    .controller('MyLunchCheckController', MyLunchCheckController);
-     
-//     MyLunchCheckController.$inject  =  ['$scope'];
-//     function MyLunchCheckController($scope) {
-//       $scope.MenuInput  = [];
-//      // $scope.MenuInput.splice(,);
-//       $scope.msg1  =   "Enjoy!";
-//       $scope.msg2  =   "Too much!";
-       
-//       $scope.checkInput  =  function() {
-//        // for (var i = 0; i < $scope.values; i++) {
-//           if($scope.MenuInput.length <= 3) {
-//           //if($scope.values <= 3) {
-//             return $scope.msg1;
-//              }else{
-//             return $scope.msg2;
-//              }
-//           }
-//       };   
-      
-// })();
 !function(){"use strict";function a(a){a.MenuInput=[],a.msg1="Enjoy!",a.msg2="Too much!",a.msg3="Enter data first",a.checkInput=function(){a.MenuInput.split(",").length<=-1?a.values=a.msg3:a.MenuInput.split(",").length<=3?a.values=a.msg1:a.values=a.msg2}}angular.module("LunchChecker",[]).controller("MyLunchCheckController",a),a.$inject=["$scope"]}();
