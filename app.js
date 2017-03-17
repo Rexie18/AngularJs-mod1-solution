3d6010f855c21cd4eb2e4b6f3ad23c8e5cd95fde
Rexie18 <ro@students.kiron.ngo>
2017-03-17 11:10:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,48 +1,52 @@
 
  (function () {
-//     'use strict';
 
-// angular.module('LunchChecker', [])
-//   .controller('MyLunchCheckController', ['$scope', function($scope) {
-//     $scope.msg1  =   "Enjoy!";
-//      $scope.msg2  =   "Too much!";
-
-//      $scope.checkInput  =  function() {
-//             if($scope.MenuInput.split(",").length <= 3) {
-//                 $scope.values = $scope.msg1;
-//             } else{
-//                 $scope.values = $scope.msg2;
-//             }
-//          }
-//     }  
-// })();
-
-
-
-    'use strict';
+'use strict';
      
     angular.module('LunchChecker', [])
    .controller('MyLunchCheckController', MyLunchCheckController);
      
     MyLunchCheckController.$inject  =  ['$scope'];
     function MyLunchCheckController($scope) {
-      $scope.MenuInput  = [];
-     // $scope.MenuInput.splice(,);
-      $scope.msg1  =   "Enjoy!";
-      $scope.msg2  =   "Too much!";
-       
-      $scope.checkInput  =  function() {
-       // for (var i = 0; i < $scope.values; i++) {
-          if($scope.MenuInput.length <= 3) {
-          //if($scope.values <= 3) {
-            return $scope.msg1;
-             }else{
-            return $scope.msg2;
-             }
-          }
-      };   
-      
+    $scope.msg1  =   "Enjoy!";
+     $scope.msg2  =   "Too much!";
+
+     $scope.checkInput  =  function() {
+            if($scope.MenuInput.split(",").length <= 3) {
+                $scope.values = $scope.msg1;
+            } else{
+                $scope.values = $scope.msg2;
+            }
+         }
+    }  
 })();
+
+
+
+//     'use strict';
+     
+//     angular.module('LunchChecker', [])
+//    .controller('MyLunchCheckController', MyLunchCheckController);
+     
+//     MyLunchCheckController.$inject  =  ['$scope'];
+//     function MyLunchCheckController($scope) {
+//       $scope.MenuInput  = [];
+//      // $scope.MenuInput.splice(,);
+//       $scope.msg1  =   "Enjoy!";
+//       $scope.msg2  =   "Too much!";
+       
+//       $scope.checkInput  =  function() {
+//        // for (var i = 0; i < $scope.values; i++) {
+//           if($scope.MenuInput.length <= 3) {
+//           //if($scope.values <= 3) {
+//             return $scope.msg1;
+//              }else{
+//             return $scope.msg2;
+//              }
+//           }
+//       };   
+      
+// })();
 
 
 // (function () {
